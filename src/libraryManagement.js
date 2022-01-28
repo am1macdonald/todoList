2ca3eefaf3bef82c18b5464fa7bb75fb43aa855a
am1macdonald <am1macdonald@gmail.com>
2022-01-28 00:29:58 -0500

    removing development scaffolding

--- src/libraryManagement.js
+++ src/libraryManagement.js
@@ -3,16 +3,7 @@ import Project from './projectClass.js';
 import Task from './taskClass.js';
 
 export const taskLibrary = (() => {
-    let arr = [    
-      {"title":"6 task","description":"More stuff","dueDate":"2026-12-30","priority":"1","notes":"","checklist":{},"identifier":16408544564564},      
-      {"title":"3","description":"123123","dueDate":"2023-12-30","priority":"1","notes":"","checklist":{},"identifier":1640854693381},
-      {"title":"4","description":"1231231231","dueDate":"2024-12-30","priority":"1","notes":"","checklist":{},"identifier":1640854696682},
-      {"title":"7","description":"123123","dueDate":"2027-12-30","priority":"1","notes":"","checklist":{},"identifier":1640854645645645},
-      {"title":"5 task","description":"stuff","dueDate":"2025-12-30","priority":"1","notes":"","checklist":{},"identifier":164085464564564},      
-      {"title":"second task","description":"More stuff","dueDate":"2022-12-30","priority":"1","notes":"","checklist":{},"identifier":1640854644789},
-      {"title":"8","description":"1231231231","dueDate":"2028-12-30","priority":"1","notes":"","checklist":{},"identifier":164085465675675},
-      {"title":"first task","description":"stuff","dueDate":"2021-12-31","priority":"1","notes":"","checklist":{},"identifier":1640854644788}
-    ];
+    let arr = [];
     if (window.localStorage.getItem('task-library')) {
       arr = JSON
       .parse(window.localStorage
