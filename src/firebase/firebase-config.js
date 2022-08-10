4c5b89174f851c7d81922b53a04834fa052a4b40
Adam MacDonald <am1macdonald@gmail.com>
2022-08-09 20:51:51 -0400

    add firebase config

---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,35 @@
+import { initializeApp } from 'firebase/app';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBZHP4QGn01AWEfD4kdtWgCKbmzXiMpWdA",
+
+  authDomain: "todo-29c0d.firebaseapp.com",
+
+  projectId: "todo-29c0d",
+
+  storageBucket: "todo-29c0d.appspot.com",
+
+  messagingSenderId: "78402583426",
+
+  appId: "1:78402583426:web:670ee4168824147e322d9b"
+
+};
+
+const getFirebaseConfig = () => {
+  if (!firebaseConfig || !firebaseConfig.apiKey) {
+    throw new Error(
+      "No Firebase configuration object provided." +
+        "\n" +
+        "Add your web app's configuration object to firebase-config.js"
+    );
+  } else {
+    return config;
+  }
+}
+
+
+const app = initializeApp(firebaseConfig);
+
+export { getFirebaseConfig }
