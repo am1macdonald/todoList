473c68c9b9462180ea71b2a429024a52b304ddc2
Adam MacDonald <am1macdonald@gmail.com>
2024-05-03 14:41:30 -0400

    add 'get user by email' query

--- server/internal/database/users.sql.go
+++ server/internal/database/users.sql.go
@@ -33,3 +33,22 @@ func (q *Queries) CreateUser(ctx context.Context, arg CreateUserParams) (User, e
 	)
 	return i, err
 }
+
+const getUserFromEmail = `-- name: GetUserFromEmail :one
+SELECT id, name, email, created_at, updated_at, last_login from users
+where email = ?
+`
+
+func (q *Queries) GetUserFromEmail(ctx context.Context, email string) (User, error) {
+	row := q.db.QueryRowContext(ctx, getUserFromEmail, email)
+	var i User
+	err := row.Scan(
+		&i.ID,
+		&i.Name,
+		&i.Email,
+		&i.CreatedAt,
+		&i.UpdatedAt,
+		&i.LastLogin,
+	)
+	return i, err
+}
--- server/sql/queries/users.sql
+++ server/sql/queries/users.sql
@@ -2,3 +2,7 @@
 INSERT INTO users (name, email)
 VALUES (?, ?)
 RETURNING *;
+
+-- name: GetUserFromEmail :one
+SELECT * from users
+where email = ?;
