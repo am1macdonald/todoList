713f577efe08dc7645aff6f74d1a76081d6c3218
Adam MacDonald <am1macdonald@gmail.com>
2024-05-04 16:06:06 -0400

    Refactor token generation

--- server/internal/user/user.go
+++ server/internal/user/user.go
@@ -22,40 +22,30 @@ type UserClaims struct {
 	jwt.RegisteredClaims
 }
 
-func (u *User) GetAccessToken(expiresIn time.Duration) (string, error) {
-	claims := UserClaims{
-		u,
-		jwt.RegisteredClaims{
-			Issuer:    "passporter_access",
-			Subject:   u.Email,
-			ID:        uuid.New().String(),
-			ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
-			IssuedAt:  jwt.NewNumericDate(time.Now()),
-			NotBefore: jwt.NewNumericDate(time.Now()),
-		}}
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	ss, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
-	if err != nil {
-		return "", err
-	}
-	return ss, nil
+func (u *User) GetAccessToken() (string, error) {
+	return u.genToken("passporter_access", time.Now().Add(time.Duration(time.Hour*24)))
 }
 
 func (u *User) GetRefreshToken() (string, error) {
+	return u.genToken("passporter_refresh", time.Now().Add(time.Duration(time.Hour*24*30)))
+}
+
+func (u *User) GetLoginToken() (string, error) {
+	return u.genToken("passporter_login", time.Now().Add(time.Duration(time.Minute*5)))
+}
+
+func (u *User) genToken(issuer string, expiry time.Time) (string, error) {
 	claims := UserClaims{
 		u,
 		jwt.RegisteredClaims{
-			Issuer:    "passporter_refresh",
+			Issuer:    issuer,
 			Subject:   u.Email,
 			ID:        uuid.New().String(),
-			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Duration(time.Hour * 24))),
+			ExpiresAt: jwt.NewNumericDate(expiry),
 			IssuedAt:  jwt.NewNumericDate(time.Now()),
 			NotBefore: jwt.NewNumericDate(time.Now()),
 		}}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	ss, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
-	if err != nil {
-		return "", err
-	}
-	return ss, nil
+	return token.SignedString([]byte(os.Getenv("JWT_SECRET")))
 }
+
