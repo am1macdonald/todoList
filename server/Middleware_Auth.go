f803e15f3c1318f208725843ab05cbae92074e57
Adam MacDonald <am1macdonald@gmail.com>
2024-05-08 07:47:45 -0400

    Implement auth middleware function

--- server/Middleware_Auth.go
+++ server/Middleware_Auth.go
@@ -2,22 +2,30 @@ package main
 
 import (
 	"net/http"
+	"strings"
 
-	"github.com/am1macdonald/to-do-list/server/internal/database"
+	"github.com/am1macdonald/to-do-list/server/internal/user"
 )
 
-type authenticatedHandler func(w http.ResponseWriter, r *http.Request, u *database.User)
+type authenticatedHandler func(w http.ResponseWriter, r *http.Request, u *user.User)
 
-func MiddlewareAuthenticate(next authenticatedHandler) http.Handler {
+func MiddlewareAuthenticate(next authenticatedHandler) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		u := &database.User{}
-		next(w, r, u)
-	})
-}
-
-func MiddlewareAuthorize(next authenticatedHandler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		u := &database.User{}
+		t := r.Header.Get("Authorization")
+		if t == "" {
+			jsonResponse(w, 400, "authorization header is required")
+			return
+		}
+		token := strings.Split(t, " ")
+		if len(token) < 2 || strings.ToLower(token[0]) != "bearer" {
+			jsonResponse(w, 400, "authorization is invalid")
+			return
+		}
+		u, issuer, err := user.UserFromToken(token[1])
+		if err != nil || strings.ToLower(issuer) != "passporter_access" {
+			jsonResponse(w, 400, "token is invalid")
+			return
+		}
 		next(w, r, u)
 	})
 }
