107fe9e0bf19d531de4efc2a4eb343e2e97b98d4
Adam MacDonald <am1macdonald@gmail.com>
2024-05-13 07:47:50 -0400

    Add tasks queries

---
+++ server/sql/queries/tasks.sql
@@ -0,0 +1,24 @@
+-- name: AddTask :one
+INSERT INTO tasks (user_id, project_id, title, description, notes, priority, checklist, deadline, complete)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+RETURNING id, title, description, notes, priority, checklist, deadline, complete;
+
+-- name: GetUserTasks :many
+SELECT * from tasks
+where user_id = ?;
+
+-- name: UpdateTask :one
+UPDATE tasks 
+set title = ?, 
+  description = ?, 
+  notes = ?, 
+  priority = ?,
+  checklist = ?,
+  deadline = ?, 
+  complete = ?,
+  updated_at = CURRENT_TIMESTAMP
+where id = ?
+RETURNING id;
+
+-- name: DeleteTask :exec
+DELETE FROM tasks where id = ?;
