43e2de374d7fc878bfae6c599246423ae333f411
Adam MacDonald <am1macdonald@gmail.com>
2024-05-16 07:22:06 -0400

    Add session 'new' method

--- server/internal/session/session.go
+++ server/internal/session/session.go
@@ -2,6 +2,7 @@ package session
 
 import (
 	"time"
+
 	"github.com/google/uuid"
 )
 
@@ -15,8 +16,16 @@ type Session struct {
 	Data *SessionData
 }
 
-func New() (*Session, error) {
-	return nil, nil
+func New(userID int) *Session {
+	sd := SessionData{
+		Exprires: time.Now().Add(time.Hour * 24).Unix(),
+		UserID:   int64(userID),
+	}
+	s := Session{
+		Key:  "todo:session:" + uuid.New().String(),
+		Data: &sd,
+	}
+	return &s
 }
 
 func FromToken(token string) (*Session, error) {
