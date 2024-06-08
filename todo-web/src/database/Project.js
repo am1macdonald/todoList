b1e007689fe4b2297cef70fdf8967eb3cfe8893f
Adam MacDonald <am1macdonald@gmail.com>
2024-06-08 07:14:35 -0400

    Fix var naming

--- todo-web/src/database/Project.js
+++ todo-web/src/database/Project.js
@@ -1,19 +1,22 @@
 /**
  * @param {Project} project
+ * @return {{notes: *, description, title, deadline: number, complete: *}}
+ */
+function projectToDbProject(project) {
+  return {
+    title: project.title,
+    description: project.description,
+    notes: project.notes,
+    deadline: Math.floor(new Date(project.date).getTime() / 1000),
+    complete: project.complete
+  };
+}
+
+/**
+ * @param {Project} project
  * @param {AppConfig} appConfig
  */
 export async function sendProjectToDatabase(appConfig, project) {
-
-  function projectToDbProject(project) {
-    return {
-      title: project.title,
-      description: project.description,
-      notes: project.notes,
-      deadline: Math.floor(new Date(project.date).getTime() / 1000),
-      complete: project.complete
-    };
-  }
-
   const response = await fetch(`/api/v1/${appConfig.session.userID}/projects`, {
     method: "POST",
     body: JSON.stringify(
@@ -26,8 +29,8 @@ export async function sendProjectToDatabase(appConfig, project) {
   return response.json();
 }
 
-export async function deleteProjectFromDatabase(appConfig, taskID) {
-  const response = await fetch(`/api/v1/${appConfig.session.userID}/projects/${taskID}`, {
+export async function deleteProjectFromDatabase(appConfig, projectID) {
+  const response = await fetch(`/api/v1/${appConfig.session.userID}/projects/${projectID}`, {
     method: "DELETE"
   });
   if (!response.ok && response.status !== 200) {
