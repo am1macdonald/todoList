a12694597fe9809467427e4ab73f84a968c78079
Adam MacDonald <am1macdonald@gmail.com>
2024-06-12 07:49:51 -0400

    Implement update method

--- todo-web/src/database/Task.js
+++ todo-web/src/database/Task.js
@@ -1,24 +1,25 @@
 /**
+ * @param {Task}task
+ * @return {{notes: *, description, title, deadline: number, complete: *}}
+ */
+function taskToDbTask(task) {
+  return {
+    id: task.id ?? undefined,
+    title: task.title,
+    description: task.description,
+    priority: task.priority,
+    notes: task.notes,
+    deadline: task.deadline,
+    complete: task.complete
+  };
+}
+
+/**
  * @param {Task} task
  * @param {AppConfig} appConfig
  */
 export async function sendTaskToDatabase(appConfig, task) {
 
-  /**
-   * @param {Task}task
-   * @return {{notes: *, description, title, deadline: number, complete: *}}
-   */
-  function taskToDbTask(task) {
-    return {
-      id: task.id ?? undefined,
-      title: task.title,
-      description: task.description,
-      priority: task.priority,
-      notes: task.notes,
-      deadline: Math.floor(new Date(task.deadline).getTime() / 1000),
-      complete: task.complete
-    };
-  }
 
   const response = await fetch(`/api/v1/${appConfig.session.userID}/tasks`, {
     method: "POST",
@@ -38,6 +39,24 @@ export async function deleteTaskFromDatabase(appConfig, taskID) {
   });
   if (!response.ok && response.status !== 200) {
     throw new Error("failed to delete task");
+  }
+  return response.json();
+}
+
+/**
+ * @param {AppConfig} appConfig
+ * @param {Task} task
+ * @return {Promise<any>}
+ */
+export async function updateDatabaseTask(appConfig, task) {
+  const response = await fetch(`/api/v1/${appConfig.session.userID}/tasks/${task.id}`, {
+    method: "PUT",
+    body: JSON.stringify(
+      taskToDbTask(task)
+    )
+  });
+  if (!response.ok && response.status !== 200) {
+    throw new Error("failed to update task");
   }
   return response.json();
 }
